511f224f7bc7766800c71ef24cc54f7ca853ed88
GahelG <143622147+GahelG@users.noreply.github.com>
2023-09-06 08:45:38 -0600

    Add files via upload

---
+++ ListaTareas.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package listatareas;
+
+/**
+ *
+ * @author Usuario
+ */
+public class ListaTareas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String tarea1 = "Lavar los trastes";
+        String tarea2 = "Tender tu cama";
+        String tarea3 = "Hacerte el desayuno";
+        String tarea4 = "Baniarte";
+        String tarea5 = "Preparar tu mochila para la escuela";
+        
+        
+        
+        
+        String cadenaTExto = "cadena de texto";
+        
+        
+        
+       
+        
+                
+        System.out.println("LISTA DE TAREAS :" +tarea1 + ","+tarea2 +"," +tarea3 +"," +tarea4 + ","+tarea5 );
+       
+        // TODO code application logic here
+    }
+    
+}
