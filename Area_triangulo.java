196ceab2384ef93d75b3c44b969059e34bea2179
GahelG <143622147+GahelG@users.noreply.github.com>
2023-09-08 08:43:00 -0600

    Add files via upload

---
+++ Area_triangulo.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package areatriangulo;
+
+/**
+ *
+ * @author Usuario
+ */
+public class AreaTriangulo {
+
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int base = 5 ;
+        int altura = 7;
+        int bxh = base * altura;
+        double area = bxh / 2 ;
+        
+        System.out.println("El area del triangulo es :" + area);
+    }
+    
+}
